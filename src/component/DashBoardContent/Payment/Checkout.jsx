caef8c7329bb1a8874e0a87a7654d59c45067acf
unknown <anik.fahim07@gmail.com>
2023-10-17 12:16:23 +0600

    payment system implemented

--- src/component/DashBoardContent/Payment/Checkout.jsx
+++ src/component/DashBoardContent/Payment/Checkout.jsx
@@ -1,11 +1,40 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../../providers/AuthProvider';
 
 
-const Checkout = () => {
+
+const Checkout = ({price,selectedCourse,refetch}) => {
+
   const [cardError, setCardError] = useState('')
+  const [clientSecret, setClientSecret] = useState('')
+  const [processing,setProcessing]= useState(false)
+  const [transactionId, setTransactionId] = useState('');
     const stripe = useStripe();
     const elements = useElements()
+    const {user}= useContext(AuthContext)
+
+
+  useEffect(()=>{
+    if(price>0){
+      fetch('http://localhost:5000/create-payment-intent',
+    
+    {
+  method: 'POST',
+  headers:{
+    'content-type': 'application/json'
+  },
+  body: JSON.stringify({price})
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      // console.log(data)
+      setClientSecret(data.clientSecret)
+    })
+    }
+  },[price])
+
+
     const handleSubmit = async(e)=>{
         e.preventDefault()
       if(!stripe || !elements){
@@ -15,7 +44,7 @@ const Checkout = () => {
       if(card === null){
         return
       }
-      const {error,paymentMethod}= await stripe.createPaymentMethod({
+      const {error}= await stripe.createPaymentMethod({
         type:'card',
         card
       })
@@ -25,12 +54,60 @@ const Checkout = () => {
       }
 else{
   setCardError('')
-    console.log('payment method', paymentMethod)
+   
 }
+setProcessing(true)
+const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+  clientSecret,
+  {
+      payment_method: {
+          card: card,
+          billing_details: {
+              email: user?.email || 'unknown',
+              name: user?.displayName || 'anonymous'
+          },
+      },
+  },
+);
+
+if(confirmError){
+  console.log(confirmError);
+}
+console.log(paymentIntent)
+setProcessing(false)
+if(paymentIntent.status === 'succeeded'){
+  setTransactionId(paymentIntent.id)
+  const payment = {
+    email:user?.email,
+    transactionId:paymentIntent.id,
+    availableSeats:selectedCourse.map(course=>course.availableSeats -1),
+    price,
+    date:new Date(),
+    quantity:selectedCourse.length,
+    courses:selectedCourse.map(course=>course.name),
+    coursesId:selectedCourse.map(course=>course._id)
+  }
+
+  fetch('http://localhost:5000/payment',{
+    method:'POST',
+    headers:{
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(payment)
+  })
+  .then(res=>res.json())
+  .then(data=>{
+    console.log(data)
+    // notify here
+    refetch()
+  })
+
+}
+
     }
     return (
         <>
-        <form className='w-2/3' onSubmit={handleSubmit}>
+        <form className='w-full md:w-2/3' onSubmit={handleSubmit}>
         <CardElement
           options={{
             style: {
@@ -47,11 +124,12 @@ else{
             },
           }}
         />
-        <button className='btn btn-outline btn-primary btn-sm mt-2' type="submit" disabled={!stripe}>
+        <button className='btn btn-outline btn-primary btn-sm mt-2' type="submit" disabled={!stripe || !clientSecret || processing || transactionId}>
           Pay
         </button>
       </form>
       {cardError && <p className='text-red-600'>{cardError}</p>}
+      {transactionId && <span className='text-green-500'>Transaction completed with transaction id: <span className='text-red-500'>{transactionId}</span></span>}
       </>
     );
 };
--- src/component/DashBoardContent/Payment/Payment.jsx
+++ src/component/DashBoardContent/Payment/Payment.jsx
@@ -8,13 +8,15 @@ import useSelectCourse from "../../../Hook/useSelectedCourse";
 const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_PK);
 
 const Payment = () => {
-    const [selectedCourse] = useSelectCourse();
+    const [selectedCourse, refetch] = useSelectCourse();
+  
     const total = selectedCourse.reduce((sum, course) => course.price + sum, 0);
+    const price = parseFloat(total.toFixed(2))
     return (
         <div className='max-h-screen mx-auto '>
             <div className="p-20">
           
-            <Elements stripe={stripePromise}> <Checkout/></Elements>
+            <Elements stripe={stripePromise}> <Checkout selectedCourse={selectedCourse} refetch={refetch} price={price}/></Elements>
             </div>
            
         </div>
